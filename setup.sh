625ca6c781f330d08afa783b197ab0f9c3a69e2c
Zhaocheng Che <chezhaocheng@outlook.com>
2023-06-04 16:07:39 -0400

    Add tmux conf

--- setup.sh
+++ setup.sh
@@ -11,9 +11,13 @@ function addf() {
 		echo "creating ${1}"
 		touch ${1}
 	fi
-	echo "writing to ${1}"
-	echo -e "\n${2}\n" >> ${1}
-	echo "done writing ${1}"
+	if grep -q ${3} ${1}; then
+		echo "${3} is already setup"
+	else
+		echo "writing to ${1}"
+		echo -e "\n${2}\n" >> ${1}
+		echo "done writing ${1}"
+	fi
 }
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -21,16 +25,19 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 ### vimrc
 SCRIPT="${SCRIPT_DIR}/vimrc"
-addf "${HOME}/.vimrc" ":if filereadable(expand(\"${SCRIPT}\")) \n\t:source ${SCRIPT}\n:endif" 
+addf "${HOME}/.vimrc" ":if filereadable(expand(\"${SCRIPT}\")) \n\t:source ${SCRIPT}\n:endif" ${SCRIPT}
 
 ### bashrc
 SCRIPT="${SCRIPT_DIR}/bashrc"
-addf "${HOME}/.bashrc" "if [[ -f ${SCRIPT} ]]; then \n\t source ${SCRIPT} \nfi"
+addf "${HOME}/.bashrc" "if [[ -f ${SCRIPT} ]]; then \n\t source ${SCRIPT} \nfi" ${SCRIPT}
 
 ### zshrc
 SCRIPT="${SCRIPT_DIR}/bashrc"
-addf "${HOME}/.zshrc" "if [[ -f ${SCRIPT} ]]; then \n\t source ${SCRIPT} \nfi"
+addf "${HOME}/.zshrc" "if [[ -f ${SCRIPT} ]]; then \n\t source ${SCRIPT} \nfi" ${SCRIPT}
 
+### tmux.conf
+SCRIPT="${SCRIPT_DIR}/tmux.conf"
+addf "${HOME}/.tmux.conf" "source-file ${SCRIPT}" ${SCRIPT}
 
 
 
